25bca98d60f20f7f03aeef838b0ef83e5fc5e51c
maycon douglas <macondouglasniculau@gmail.com>
2024-01-02 09:10:56 -0300

    Praticando-Python

---
+++ main.py
@@ -0,0 +1,47 @@
+
+"""""
+
+name = input("Michel")
+suarname = input("Douglas")
+age = int(input(20))
+
+print(name)
+print(suarname)
+print(type(age))
+
+"""
+
+""""
+
+soma = 1+1
+multiplicaçao = 2 * 2
+divisao = 30 / 3
+potencia = 7**2
+
+print("soma", soma)
+print("multiplicaçao", multiplicaçao)
+print("divisao", divisao)
+
+"""
+
+"""""
+
+idade = int( input("Informe sua idade:") ) 
+
+if idade >= 18:
+    print("Permetido!")
+else:
+    print("Bloqueado!")
+
+"""
+
+Salario = float( input("Informe o salário:  "))
+
+if Salario <= 3000:
+    print(" Programador Junior")
+elif Salario > 3000 and Salario <= 6000:
+    print(" Programador pleno")
+elif Salario > 6000 and Salario <= 15000:
+    print(" Programador Senior")
+else:
+     print(" Gerente de projetos")
