d7dc3d46c61b6d11276cc0c4633f201997337acf
maycon douglas <macondouglasniculau@gmail.com>
2024-01-07 20:45:09 -0300

    PraticandoPython

---
+++ aula1.py
@@ -0,0 +1,7 @@
+
+
+name = "Michael"
+surname = "Douglas"
+age = 20
+
+print(f"My name is {name} and I {age}")
---
+++ aula3.py
@@ -0,0 +1,8 @@
+
+# Tipos de dados Number string booleano
+
+# String
+print("Michael")
+
+# Number
+print(20)
